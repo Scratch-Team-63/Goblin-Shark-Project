2ad5df25b68fc2933d694ce00efbc92d0bf5a9d2
Adam Kim <adamkim1006@gmail.com>
2024-02-17 16:15:31 -0800

    added drop down functionality
    Co-authored-by: Adam Kim <alexanderjosephdavid@gmail.com>
    Co-authored-by: Ron Garcia <josue_role@yahoo.com>

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,14 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {NextUIProvider} from "@nextui-org/react";
 import "tailwindcss/tailwind.css";
 import UserForm from './UserForm.jsx';
 
+
 function App (){
+  const [displayData, setDisplayData] = useState(null);
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const handleSearch = async (cuisine, distance, budget, latitude, longitude) => {
+    try {
+      setErrorMessage('');
+      console.log('User input successfully passed into App.jsx and about to make request to Server.js', )
+      // Make the API request with the constructed optionSymbol
+      const response = await axios.get(`/api/search/${cuisine}/${distance}/${budget}/${latitude}/${longitude}`);
+      console.log(response.data);
+      setDisplayData(response.data);
+    } catch (error) {
+      console.error('Error fetching restaurant data:', error);
+      setDisplayData(null);
+      setErrorMessage('No contract found with given parameters.')
+    }
+  };
 
   return (
     <NextUIProvider>
       <h2>Hello! App works!</h2>
-      <UserForm/>
+      <UserForm hitSearch={handleSearch}/>
     </NextUIProvider>
   );
 }
--- client/src/components/UserForm.jsx
+++ client/src/components/UserForm.jsx
@@ -1,12 +1,29 @@
-import React from 'react'
-import {Button, ButtonGroup} from "@nextui-org/react";
+import React, { useState } from 'react'
+import {Input, Button, ButtonGroup} from "@nextui-org/react";
+import DropDown from './DropDown.jsx';
 
+export default function UserForm ({ hitSearch }) {
+  const [cuisine, setCuisine] = useState('');
+  const [distance, setDistance] = useState('');
+  const [budget, setBudget] = useState('');
+  // const [userInput, setUserInput] = useState('');
+  // const [userInput, setUserInput] = useState('');
+  // const [userInput, setUserInput] = useState('');
 
-export default function UserForm () {
+  const handleSubmit = (e) => {
+    // e.preventDefault();
+    console.log('Submitting and about to make request to Server.js', cuisine, distance, budget);
+    hitSearch(cuisine, distance, budget);
+  };
 
   return (
     <div>
-      <Button radius="full" className="bg-gradient-to-tr from-pink-500 to-yellow-500 text-white shadow-lg" size="md" >Click me!</Button>
+      <DropDown setCuisine={setCuisine}/>
+      {/* <Input onChange={(e) => setCuisine(e.target.value)} size="md" label="Whatcha in the mood for?" labelPlacement="outside"placeholder="cuisine (i.e. Italian, Mexican, Chinese)"/> */}
+      <Input onChange={(e) => setDistance(e.target.value)} size="md" label="How close does it need to be?" labelPlacement="outside" placeholder="distance (in miles)"/>
+      {/* add maximum distance */}
+      <Input onChange={(e) => setBudget(e.target.value)} size="md" label="How much are you tryna spend?" labelPlacement="outside" placeholder="budget (in USD)"/>
+      <Button onPress={handleSubmit}radius="full" className="bg-gradient-to-tr from-pink-500 to-yellow-500 text-white shadow-lg" size="md" >Find Restaurants!</Button>
     </div>
   )
 }
