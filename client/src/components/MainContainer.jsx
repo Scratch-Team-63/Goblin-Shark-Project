599869acb87375f082da7eee47edd7d0b5982a83
Adam Kim <adamkim1006@gmail.com>
2024-02-19 12:10:51 -0800

    working mvp--displaying fetched data
    > Co-authored-by: Alexander David <alexanderjosephdavid@gmail.com>
    > Co-authored-by: Ron Garcia <josue_role@yahoo.com>

--- client/src/components/MainContainer.jsx
+++ client/src/components/MainContainer.jsx
@@ -1,7 +1,33 @@
-import react from 'react';
+import React, { useState } from 'react';
+import DisplayContainer from './DisplayContainer.jsx'
+import UserForm from './UserForm.jsx';
+import axios from 'axios'
 
 export default function MainContainer () {
+
+    const [displayData, setDisplayData] = useState([]);
+    const [errorMessage, setErrorMessage] = useState('');
+  
+    const handleSearch = async (cuisine, distance, budget, latitude, longitude) => {
+      try {
+        setErrorMessage('');
+        console.log('User input successfully passed into App.jsx and about to make request to Server.js', )
+        // Make the API request with the constructed optionSymbol
+        const response = await axios.get(`/api/search/${cuisine}/${distance}/${budget}/${latitude}/${longitude}`);
+        console.log(response)
+        console.log(response.data);
+        setDisplayData(response.data);
+      } catch (error) {
+        console.error('Error fetching restaurant data:', error);
+        setDisplayData([]);
+        setErrorMessage('No contract found with given parameters.')
+      }
+    };
+
     return(
-        
+        <div>
+            <UserForm hitSearch={handleSearch}/>
+            <DisplayContainer fetchedData={displayData}/>
+        </div>
     )
 }
