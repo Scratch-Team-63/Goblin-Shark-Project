d827dce625dbc6ebd47b7d0ef2259a9c39bf00b5
DanielGaran02 <156254918+DanielGaran02@users.noreply.github.com>
2024-02-21 19:01:15 -0500

    connecting frontend/backend (#3)
    * Linked database to backend
    
    * updated frontend
    
    * new changes
    
    ---------
    
    Co-authored-by: Ronal Garcia <ronalgarcia@Ronals-MBP.attlocal.net>

---
+++ client/src/components/Login.jsx
@@ -0,0 +1,70 @@
+import React, { useState, Component } from 'react';
+import { useNavigate, Link } from 'react-router-dom';
+import { Box, Button, Flex, FormControl, FormLabel, Input, VStack, Text } from "@chakra-ui/react";
+
+
+const Login = () => {
+  const BACKEND_URL = 'http://localhost:3000';
+  const [username, setUsername] = useState('');
+  const[password, setPassword] = useState('');
+  const [show, setshow] = useState(false);
+  const[isUsernameEmpty, setUsernameEmpty] = useState(false);
+  const [isPasswordEmpty, setPasswordEmpty] = useState(false);
+  const navigate = useNavigate()
+
+
+  const handleView = () => setshow(!show);
+  const handleLogin = event => {
+    event.preventDefault();
+    if(!username) setUsernameEmpty(true);
+    if(!password) setPasswordEmpty(true);
+    if(!isUsernameEmpty && !isPasswordEmpty){
+ fetch(BACKEND_URL + '/user/signIn', {
+  method:'POST',
+  headers: {
+    'Content-Type' : 'application/json',
+  },
+  body: JSON.stringify({username, password}),
+ })
+
+ .then(res => res.json())
+ .then(data => {
+  console.log(data);
+  if(data.username){
+    navigate('/main')
+  }
+ })
+ .catch(err => console.log('Login fetch /sigin: Error:', err));
+    }
+  };
+  const handleSignUp = event => {
+    event.preventDefault();
+    navigate('/signup')
+  };
+
+  return (
+    <Flex minHeight="100vh" alignItems="center" justifyContent="center">
+      <Box borderWidth={1} borderRadius="lg" p={8}>
+        <VStack spacing={4}>
+          <form onSubmit={handleLogin}>
+            <FormControl id="username">
+              <FormLabel fontSize="lg">Username</FormLabel>
+              <Input fontSize="lg" type="text" value={username} onChange={e => setUsername(e.target.value)} />
+            </FormControl>
+            <FormControl id="password">
+              <FormLabel fontSize="lg">Password</FormLabel>
+              <Input fontSize="lg" type="password" value={password} onChange={e => setPassword(e.target.value)} />
+            </FormControl>
+            <Button colorScheme="teal" type="submit" width="full">Log in</Button>
+          </form>
+          <Text fontSize="lg">Don't have an account?</Text>
+          <Box borderBottom="1px solid" borderColor="gray.500">
+            <Link to="/signup">Sign up</Link>
+          </Box>
+        </VStack>
+      </Box>
+    </Flex>
+  );
+}
+
+export default Login;
--- server/server.js
+++ server/server.js
@@ -1,12 +1,26 @@
 const express = require("express");
 const axios = require("axios");
 const path = require("path");
+const UserController = require('./UserController/UserController.js')
+
+const mongoose = require('mongoose');
 
 const apiController = require('./controllers/apiController.js')
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+const mongoURI = 'mongodb+srv://josuerole:Josue5573@ronalgarcia.nskkhq5.mongodb.net/?retryWrites=true&w=majority&appName=RonalGarcia'
+mongoose.connect(mongoURI);                              
 
+const userRouter = express.Router();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+app.use('/user', userRouter );
+userRouter.post('/signUp', UserController.createUser);
+userRouter.post('/signIn', UserController.verifyUser);
+// userRouter.post('/', UserController.createUser);
 // Serve static files from the 'dist' directory
 app.use(express.static(path.join(__dirname, "client", "dist")));
 
@@ -27,12 +41,19 @@ app.get("/", (req, res) => {
 
 app.get('/api/search/:cuisine/:distance/:budget/:latitude/:longitude', apiController.formatRequestData, (req, res) => {
   const {restaurantData} = res.locals
+  console.log(restaurantData)
   return res.status(200).json(restaurantData);
 });
 
-app.use('*', (req,res) => {
-  res.status(404).send('Not Found');
-});
+//added so that routes would work
+app.get('*', (req, res) => {
+  console.log('Request for index.html recieved');
+  res.sendFile(path.join(__dirname, "..", "client", "dist", "index.html"))
+})
+
+// app.use('*', (req,res) => {
+//   res.status(404).send('Not Found');
+// });
 
 app.use((err, req, res, next) => {
   const defaultObj = {
@@ -49,3 +70,5 @@ app.use((err, req, res, next) => {
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
+
+
